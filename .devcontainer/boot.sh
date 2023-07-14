eacbea6552dbf9e806cf7158a8b88d9a812a45ba
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2023-07-14 20:05:19 +0400

    Drop mentions of vs-code

--- .devcontainer/boot.sh
+++ .devcontainer/boot.sh
@@ -1,17 +1,15 @@
 bundle install
 yarn install
 
-sudo chown -R vscode:vscode /usr/local/bundle
-
 sudo service postgresql start
 sudo service mariadb start
 sudo service redis-server start
 sudo service memcached start
 
 # Create PostgreSQL users and databases
-sudo su postgres -c "createuser --superuser vscode"
-sudo su postgres -c "createdb -O vscode -E UTF8 -T template0 activerecord_unittest"
-sudo su postgres -c "createdb -O vscode -E UTF8 -T template0 activerecord_unittest2"
+sudo su postgres -c "createuser --superuser jetbrains_gateway"
+sudo su postgres -c "createdb -O jetbrains_gateway -E UTF8 -T template0 activerecord_unittest"
+sudo su postgres -c "createdb -O jetbrains_gateway -E UTF8 -T template0 activerecord_unittest2"
 
 # Create MySQL database and databases
 cd activerecord
